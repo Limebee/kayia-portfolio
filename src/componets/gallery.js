912b5463f9a89b025aa2ed85339ec610fc45eedb
LimeBee <kjeanlouis25@gmail.com>
2023-07-04 09:27:10 -0400

    initial commit

---
+++ src/componets/gallery.js
@@ -0,0 +1,10 @@
+export default function Gallery () {
+    return(
+        <div class="grid grid-cols-4 gap-4">
+          <div><img src="" alt=""/></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+    );
+  }
