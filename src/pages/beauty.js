a14152b71a728cc65ba6e55a9f91ab1682b78d20
LimeBee <kjeanlouis25@gmail.com>
2023-07-12 09:47:20 -0400

    add photos to gallery

--- src/pages/beauty.js
+++ src/pages/beauty.js
@@ -6,88 +6,175 @@ import Image from 'next/image'
 function Gallery () {
     return(
       <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
-        <div className="-m-1 flex flex-wrap md:-m-2">
-          <div className="flex w-1/3 flex-wrap">
+        <div className="m-3 flex flex-wrap md:-m-2">
+          <div className="flex w-1/3 flex-col">
             <section className="w-full p-1 md:p-2">
-              <Image className=''
+              <Image className='bg-white transition duration-300 ease-in-out hover:opacity-40'
               src='/beauty-photo/b1.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
               height={500} />
             </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
-            <section className="w-full p-1 md:p-2 object-fill">
-              <Image 
-              src='/beauty-photo/b2.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
-              height={500} />
-            </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
             <section className="w-full p-1 md:p-2">
-              <Image 
-              src='/beauty-photo/b3.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
-              height={500} />
-            </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
-            <section className="w-full p-1 md:p-2">
-              <Image 
-              src='/beauty-photo/b4.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
-              height={500} />
-            </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
-            <section className="w-full p-1 md:p-2">
-              <Image 
-              src='/beauty-photo/b5.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
-              height={500} />
-            </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
-            <section className="w-full p-1 md:p-2">
-              <Image 
-              src='/beauty-photo/b6.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
-              height={500} />
-            </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
-            <section className="w-full p-1 md:p-2">
-              <Image 
-              src='/beauty-photo/b7.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
-              height={500} />
-            </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
-            <section className="w-full p-1 md:p-2">
-              <Image 
+              <Image className='transform hover:scale-105 duration-500 ease-in-out'
               src='/beauty-photo/b8.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
               height={500} />
             </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
             <section className="w-full p-1 md:p-2">
-              <Image 
-              src='/beauty-photo/b9.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              <Image className=''
+              src='/beauty-photo/b33.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b37.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b27.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b36.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b4.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b24.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b5.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b31.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b32.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b34.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
               height={500} />
             </section>
           </div>
-          <div className="flex w-1/3 flex-wrap">
+          <div className="flex w-1/3 flex-col">
             <section className="w-full p-1 md:p-2">
               <Image 
-              src='/beauty-photo/b10.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              src='/beauty-photo/b30.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
               height={500} />
             </section>
-          </div>
-          <div className="flex w-1/3 flex-wrap">
             <section className="w-full p-1 md:p-2">
-              <Image 
+              <Image className=''
+              src='/beauty-photo/b6.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b20.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b7.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b19.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b16.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b2.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b22.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b17.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
               src='/beauty-photo/b11.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
               height={500} />
             </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b15.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
           </div>
-          <div className="flex w-1/3 flex-wrap">
+          <div className="flex w-1/3 flex-col">
             <section className="w-full p-1 md:p-2">
               <Image 
+              src='/beauty-photo/b35.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b29.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b28.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b23.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b3.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
               src='/beauty-photo/b12.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b13.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b21.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b14.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
+              height={500} />
+            </section>
+            <section className="w-full p-1 md:p-2">
+              <Image className=''
+              src='/beauty-photo/b25.jpg' alt="woman holding foundation tube with dots of makeup on face"  width={500}
               height={500} />
             </section>
           </div>
