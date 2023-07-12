34c5dafff1d9f773f8a87a6f276096f9ab792ff5
LimeBee <kjeanlouis25@gmail.com>
2023-07-12 09:46:05 -0400

    created about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import Navigation from '../componets/navigation.js'
+import Image from 'next/image'
+
+
+
+export default function About (){
+    return(
+        <Navigation />
+        <div>
+            
+        </div>
+    )
+}
