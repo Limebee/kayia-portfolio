73b1b4a23989482a68c83f3f66acf249ed2040dc
LimeBee <kjeanlouis25@gmail.com>
2023-07-20 09:08:20 -0400

    added light theme

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,19 +5,19 @@ import Image from 'next/image'
 
 export default function About (){
     return(
-        <>
-            <nav>
+        <div data-theme="light">
+          <nav>
             <Navigation />
-            </nav>
+          </nav>
             <div class="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
-    <div class="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
-      <div class="relative z-10 lg:py-16">
-        <div class="relative h-64 sm:h-80 lg:h-full">
-        <Image src="/burch.jpg" alt="woman with camera around neck" width={400}
-              height={400} class="absolute inset-0 h-full w-full object-cover" />
-          </div>
-        </div>
-        <div class="p-8 sm:p-16 lg:p-24">
+              <div class="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
+                <div class="relative z-10 lg:py-16">
+                   <div class="relative h-64 sm:h-80 lg:h-full">
+                    <Image src="/burch.jpg" alt="woman with camera around neck" width={400}
+                    height={400} class="absolute inset-0 h-full w-full object-cover" />
+                  </div>
+               </div>
+         <div class="p-8 sm:p-16 lg:p-24">
           <h2 class="text-2xl font-bold sm:text-3xl">
             About Kayia Burch
           </h2>
@@ -30,9 +30,9 @@ export default function About (){
           Let's create something beautiful together.
           </p>
         </div>
+               </div>
+            </div>
       </div>
-      </div>
-      </>
 
     )
 }
